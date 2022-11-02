09b18b7b26adb118ee4c16b29b7a8e90918c3f30
Josh O'Leary <josholeary@J-2.local>
2022-11-02 14:08:10 +0000

    apiProb - Implemented word frequency map counter

--- Lab2/api-problem/apiProb.js
+++ Lab2/api-problem/apiProb.js
@@ -11,11 +11,29 @@ async function fetchPosts() {
       console.log(data.title);
     }
   });
+
+  let bodyData;
+  data.map((data) => {
+    bodyData += JSON.stringify(data.body);
+  });
+  let frequency = frequencyMap(bodyData);
+  console.log(frequency);
 }
 
 function wordCounter(str) {
   let arr = str.split(" ");
   return arr.filter((word) => word !== "").length;
+}
+
+function frequencyMap(str) {
+  let freqMap = str.split(" ").map((i, j) => {
+    return {
+      Word: i,
+      Frequency: str.split(" ").filter((j) => j === i).length,
+    };
+  });
+  let freqArr = Array.from(new Set(freqMap.map(JSON.stringify)));
+  return freqArr;
 }
 
 fetchPosts();
